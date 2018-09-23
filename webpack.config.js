7938e3a8f1f23ad47ac8a7d4b8b4c9915de3f767
Alemina <kobylakk@wp.pl>
2018-09-23 19:19:59 +0200

    - added image loader and restored background - added html loader, now everything is build in dist folder (also html file)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath:'/dist'
+        filename: 'bundle.js'
     },
     module: {
         rules: [
@@ -25,9 +25,30 @@ module.exports = {
                     "style-loader", // creates style nodes from JS strings
                     "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]        
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    "html-loader"
                 ]
-                
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }]
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
 }
